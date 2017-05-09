8785e2b7ab20b04988d20521a9107a6e0db455d9
Dylan <dylanrose60@gmail.com>
2017-05-09 10:59:57 -0400

    updated programs section

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
   currentNav.addClass('nav-current');
 
-  $('#one-content').addClass('section-gone');
+  $('#two-content').addClass('section-gone');
   $('#three-content').addClass('section-gone');
   $('#four-content').addClass('section-gone');
 
